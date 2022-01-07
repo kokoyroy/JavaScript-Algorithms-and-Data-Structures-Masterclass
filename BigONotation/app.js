859116582f6b3c29559fbdf5de2eb9b6841e195f
kokoyroy <kokoyroy@gmail.com>
2022-01-07 17:58:02 +0200

    added print all pairs as O(n^2)

--- BigONotation/app.js
+++ BigONotation/app.js
@@ -1,3 +1,6 @@
+const ONE_SECOND_IN_MILLISECONDS = 1000;
+
+// O(n)
 function addUpTo_method_1(n) {
   let total = 0;
   for (let i = 1; i <= n; i++) {
@@ -6,8 +9,18 @@ function addUpTo_method_1(n) {
   return total;
 }
 
+// O(1)
 function addUpTo_method_2(n) {
   return (n * (n + 1)) / 2;
+}
+
+// O(n^2)
+function printAllPairs(n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      console.log(i, j);
+    }
+  }
 }
 
 /**
@@ -17,12 +30,12 @@ function addUpTo_method_2(n) {
  * Outputs in the console the time passed to execute given function as second argument
  */
 function measureFunctionPerformance(n, callback) {
-  const ONE_SECOND_IN_MILLISECONDS = 1000;
+  const functionName = callback.name;
   const timeStamp_1 = performance.now();
   callback(n);
   const timeStamp_2 = performance.now();
   const executionTimeInSeconds = (timeStamp_2 - timeStamp_1) / ONE_SECOND_IN_MILLISECONDS;
-  console.log(`the ${callback.name} took ${executionTimeInSeconds} to finish`);
+  console.log(`the ${functionName} took ${executionTimeInSeconds} to finish`);
 }
 
 const BIG_NUMBER = 1000000000;
